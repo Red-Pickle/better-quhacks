39e5f8e546b083731904cbe3906563b997165903
Varun Sammeta <diamondblazer59@gmail.com>
2023-12-16 15:47:30 -0500

    particles

--- Assets/Scripts/Jetpack.cs
+++ Assets/Scripts/Jetpack.cs
@@ -8,18 +8,30 @@ public class Jetpack : MonoBehaviour
     Rigidbody2D rb;
     public float jetpackForce;
     public ScoreManager sm;
+    public ParticleSystem jetpackParticlesPrefab;
 
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        jetpackParticlesPrefab.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jetpackParticlesPrefab.Play();
+        }
         if (Input.GetKey(KeyCode.Space)  && sm.currentGameState!=states.DEAD && sm.currentGameState != states.TITLESCREEN)
+        {
             rb.velocity = new Vector2(0, jetpackForce);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+            jetpackParticlesPrefab.Stop();
+            
     }
 }
